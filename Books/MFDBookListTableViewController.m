1f5ccd415944c43cb451e4695d3c021ed47c4270
Adland Lee <adland[at]4mfd[dot]com>
2014-06-03 10:00:06 -0700

    add edit list feature

--- Books/MFDBookListTableViewController.m
+++ Books/MFDBookListTableViewController.m
@@ -36,6 +36,7 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
     
     self.books = [[NSMutableArray alloc] init];
     [self loadInitialData];
