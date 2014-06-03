db53429edd1e8d96e25c3b00515a3ed065c13715
Adland Lee <adland[at]4mfd[dot]com>
2014-06-02 17:30:29 -0700

    add book object

---
+++ Books/MFDBook.h
@@ -0,0 +1,17 @@
+//
+//  MFDBook.h
+//  Books
+//
+//  Created by Adland Lee on 6/2/14.
+//  Copyright (c) 2014 Adland Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFDBook : NSObject
+
+@property NSString *title;
+@property NSString *author;
+@property NSDate *copyright;
+
+@end
---
+++ Books/MFDBook.m
@@ -0,0 +1,14 @@
+//
+//  MFDBook.m
+//  Books
+//
+//  Created by Adland Lee on 6/2/14.
+//  Copyright (c) 2014 Adland Lee. All rights reserved.
+//
+
+#import "MFDBook.h"
+
+@implementation MFDBook
+
+@end
+
