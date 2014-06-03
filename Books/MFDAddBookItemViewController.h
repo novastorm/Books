2c386c6bca337c4b27f4f2d5ed5d64062fd64f30
Adland Lee <adland[at]4mfd[dot]com>
2014-06-02 17:14:21 -0700

    add initial views

---
+++ Books/MFDAddBookItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  MFDAddBookItemViewController.h
+//  Books
+//
+//  Created by Adland Lee on 6/2/14.
+//  Copyright (c) 2014 Adland Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFDAddBookItemViewController : UIViewController
+
+@end
---
+++ Books/MFDBookListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MFDBookListTableViewController.h
+//  Books
+//
+//  Created by Adland Lee on 6/2/14.
+//  Copyright (c) 2014 Adland Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFDBookListTableViewController : UITableViewController
+
+-(IBAction)createReturnToList:(UIStoryboardSegue *)segue;
+
+@end
